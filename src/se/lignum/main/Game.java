83d029bf8f6e00fdabb006f36343ab73db61770c
sebbekarlsson <sebbekarlsson97@gmail.com>
2014-05-28 13:46:59 +0200

    Adds dimension

--- src/se/lignum/main/Game.java
+++ src/se/lignum/main/Game.java
@@ -1,10 +1,15 @@
 package se.lignum.main;
 
+import java.awt.Dimension;
+
 public class Game {
 	
 	
 	public static final int WIDTH = 640;
 	public static final int HEIGHT = WIDTH / 16 * 9;
+	public static final int SCALE = 2;
+	
+	public static final Dimension SCREENSIZE = new Dimension(WIDTH*SCALE,HEIGHT*SCALE);
 	
 	public Game() {
 		
