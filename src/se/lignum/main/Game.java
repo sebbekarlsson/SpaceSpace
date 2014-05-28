c85eccd20ef9ed7823568bd849a3d4dffcf85081
sebbekarlsson <sebbekarlsson97@gmail.com>
2014-05-28 13:45:15 +0200

    Changes stuff

--- src/se/lignum/main/Game.java
+++ src/se/lignum/main/Game.java
@@ -2,11 +2,14 @@ package se.lignum.main;
 
 public class Game {
 	
+	
+	public static final int WIDTH = 640;
+	public static final int HEIGHT = WIDTH / 16 * 9;
+	
 	public Game() {
 		
 	}
 	
-	public static final int WIDTH = 640;
-	public static final int HEIGHT = WIDTH / 16 * 9;
+
 
 }
